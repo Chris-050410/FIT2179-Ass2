7625d3dd9571c05021d7b9f9de22a56f9ef09a34
Chris-050410 <clau0053@student.monash.edu>
2026-05-21 18:34:33 +0800

    Modify the dashboard structure and alignment

--- charts.js
+++ charts.js
@@ -1,44 +1,44 @@
 var vg_1 = "json/map_malaysia_html.json";
-vegaEmbed("#malaysia_map", vg_1).then(function(result) {
+vegaEmbed("#malaysia_map", vg_1, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "json/multi_line_heatmap.json";
-vegaEmbed("#heatmap_small_multi_line", vg_2).then(function(result) {
+vegaEmbed("#heatmap_small_multi_line", vg_2, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "json/scatter_cpo_ffb.json";
-vegaEmbed("#scatter_plot", vg_3).then(function(result) {
+vegaEmbed("#scatter_plot", vg_3, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_4 = "json/stacked_area_product.json";
-vegaEmbed("#stacked_area_product", vg_4).then(function(result) {
+vegaEmbed("#stacked_area_product", vg_4, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_5 = "json/small_multi_export.json";
-vegaEmbed("#small_multi_export", vg_5).then(function(result) {
+vegaEmbed("#small_multi_export", vg_5, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_6 = "json/hover_multi_line.json";
-vegaEmbed("#multi_line_state", vg_6).then(function(result) {
+vegaEmbed("#multi_line_state", vg_6, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_7 = "json/multi_line_world.json";
-vegaEmbed("#multi_line_world", vg_7).then(function(result) {
+vegaEmbed("#multi_line_world", vg_7, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_8 = "json/slope_graph.json";
-vegaEmbed("#slope_graph", vg_8).then(function(result) {
+vegaEmbed("#slope_graph", vg_8, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_9 = "json/indexed_line.json";
-vegaEmbed("#indexed_line", vg_9).then(function(result) {
+vegaEmbed("#indexed_line", vg_9, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
